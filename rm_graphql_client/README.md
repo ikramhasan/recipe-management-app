575a5ade17245f5ed388c68d6cc9fd7df93ff037
ihni7 <ihnishat1998@gmail.com>
2021-03-06 08:31:18 +0600

    Initialized repo

---
+++ rm_graphql_client/README.md
@@ -0,0 +1,6 @@
+A simple command-line application.
+
+Created from templates made available by Stagehand under a BSD-style
+[license](https://github.com/dart-lang/stagehand/blob/master/LICENSE).
+
+get-graphql-schema http://localhost:8081/v1/graphql > lib/src/graphql
---
+++ rm_graphql_client/lib/src/graphql/operations.var.gql.dart
@@ -0,0 +1,77 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
+import 'package:rm_graphql_client/src/graphql/schema.schema.gql.dart' as _i2;
+import 'package:rm_graphql_client/src/graphql/serializers.gql.dart' as _i1;
+
+part 'operations.var.gql.g.dart';
+
+abstract class GFetchRecipeListVars
+    implements Built<GFetchRecipeListVars, GFetchRecipeListVarsBuilder> {
+  GFetchRecipeListVars._();
+
+  factory GFetchRecipeListVars(
+          [Function(GFetchRecipeListVarsBuilder b) updates]) =
+      _$GFetchRecipeListVars;
+
+  static Serializer<GFetchRecipeListVars> get serializer =>
+      _$gFetchRecipeListVarsSerializer;
+  Map<String, dynamic> toJson() =>
+      _i1.serializers.serializeWith(GFetchRecipeListVars.serializer, this);
+  static GFetchRecipeListVars fromJson(Map<String, dynamic> json) =>
+      _i1.serializers.deserializeWith(GFetchRecipeListVars.serializer, json);
+}
+
+abstract class GFetchRecipeIndgredientsVars
+    implements
+        Built<GFetchRecipeIndgredientsVars,
+            GFetchRecipeIndgredientsVarsBuilder> {
+  GFetchRecipeIndgredientsVars._();
+
+  factory GFetchRecipeIndgredientsVars(
+          [Function(GFetchRecipeIndgredientsVarsBuilder b) updates]) =
+      _$GFetchRecipeIndgredientsVars;
+
+  @nullable
+  _i2.Gingredients_bool_exp get where;
+  static Serializer<GFetchRecipeIndgredientsVars> get serializer =>
+      _$gFetchRecipeIndgredientsVarsSerializer;
+  Map<String, dynamic> toJson() => _i1.serializers
+      .serializeWith(GFetchRecipeIndgredientsVars.serializer, this);
+  static GFetchRecipeIndgredientsVars fromJson(Map<String, dynamic> json) =>
+      _i1.serializers
+          .deserializeWith(GFetchRecipeIndgredientsVars.serializer, json);
+}
+
+abstract class GDeleteRecipeVars
+    implements Built<GDeleteRecipeVars, GDeleteRecipeVarsBuilder> {
+  GDeleteRecipeVars._();
+
+  factory GDeleteRecipeVars([Function(GDeleteRecipeVarsBuilder b) updates]) =
+      _$GDeleteRecipeVars;
+
+  int get id;
+  static Serializer<GDeleteRecipeVars> get serializer =>
+      _$gDeleteRecipeVarsSerializer;
+  Map<String, dynamic> toJson() =>
+      _i1.serializers.serializeWith(GDeleteRecipeVars.serializer, this);
+  static GDeleteRecipeVars fromJson(Map<String, dynamic> json) =>
+      _i1.serializers.deserializeWith(GDeleteRecipeVars.serializer, json);
+}
+
+abstract class GInsertRecipeVars
+    implements Built<GInsertRecipeVars, GInsertRecipeVarsBuilder> {
+  GInsertRecipeVars._();
+
+  factory GInsertRecipeVars([Function(GInsertRecipeVarsBuilder b) updates]) =
+      _$GInsertRecipeVars;
+
+  _i2.Grecipes_insert_input get object;
+  static Serializer<GInsertRecipeVars> get serializer =>
+      _$gInsertRecipeVarsSerializer;
+  Map<String, dynamic> toJson() =>
+      _i1.serializers.serializeWith(GInsertRecipeVars.serializer, this);
+  static GInsertRecipeVars fromJson(Map<String, dynamic> json) =>
+      _i1.serializers.deserializeWith(GInsertRecipeVars.serializer, json);
+}
